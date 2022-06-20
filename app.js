f08ef75da262eaa3f67d0f880a10ce34a318bba2
MSajedian <mohammadsajedian@gmail.com>
2022-06-20 20:01:30 +0100

    schema and BookType object created

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 import express from "express"
+import { graphqlHTTP } from "express-graphql"
 
 const app = express()
+
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        // schema: MyGraphQLSchema,
+        // graphiql: true,
+    }),
+);
 
 app.listen(4000, () => {
     console.log(`Now, server is listening on port 4000`);
