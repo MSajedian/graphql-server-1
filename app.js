b6b9226db45026dec5b15ce1868de42b71788e60
MSajedian <mohammadsajedian@gmail.com>
2022-06-21 18:11:49 +0100

    added Root Query

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from "express"
 import { graphqlHTTP } from "express-graphql"
+import { schema } from "./schema/schema.js"
 
 const app = express()
 
 app.use(
     '/graphql',
     graphqlHTTP({
+        schema
         // schema: MyGraphQLSchema,
         // graphiql: true,
     }),
--- schema/schema.js
+++ schema/schema.js
@@ -1,12 +1,30 @@
 import graphql from "graphql"
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
-    fields: ( ) => ({
+    fields: () => ({
         id: { type: GraphQLString },
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
     })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: { id: { type: GraphQLString } },
+            resolve(parent, args) {
+                // code to get data from db / other source
+
+            }
+        }
+    }
+});
+
+export const schema = new GraphQLSchema({
+    query: RootQuery
 });
