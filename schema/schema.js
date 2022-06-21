6545d6f52bcc1e1d003d2210f51ac1b057ced207
MSajedian <mohammadsajedian@gmail.com>
2022-06-21 18:26:28 +0100

    added dummy data

--- schema/schema.js
+++ schema/schema.js
@@ -2,6 +2,13 @@ import graphql from "graphql"
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
+// dummy data
+var books = [
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+];
+
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
@@ -19,7 +26,7 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLString } },
             resolve(parent, args) {
                 // code to get data from db / other source
-
+                return books.find(element => element.id === args.id);
             }
         }
     }
---
+++ test.js
@@ -0,0 +1,9 @@
+var books = [
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+];
+
+const found = books.find(element => element.id > 2);
+
+console.log('found:', found)
