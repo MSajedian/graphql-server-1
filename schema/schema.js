74dc75df8f00d708fab519ddcdcb28f5f7567761
MSajedian <mohammadsajedian@gmail.com>
2022-06-23 14:40:04 +0100

    GraphQL Lists

--- schema/schema.js
+++ schema/schema.js
@@ -5,7 +5,8 @@ const {
     GraphQLString,
     GraphQLSchema,
     GraphQLID,
-    GraphQLInt
+    GraphQLInt,
+    GraphQLList
 } = graphql;
 
 
@@ -13,8 +14,12 @@ const {
 var books = [
     { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
     { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+    { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
     { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+    { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+    { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
 ];
+
 
 var authors = [
     { name: 'Patrick Rothfuss', age: 44, id: '1' },
@@ -44,7 +49,13 @@ const AuthorType = new GraphQLObjectType({
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
-        age: { type: GraphQLInt }
+        age: { type: GraphQLInt },
+        books: {    
+            type: new GraphQLList(BookType),
+            resolve(parent, args) {
+                return books.filter(element => element.authorId === parent.id)
+            }
+        }
     })
 });
 
