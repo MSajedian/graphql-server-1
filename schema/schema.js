5d2c99dca65f104229a62bc57209c4a0eb8f74b3
MSajedian <mohammadsajedian@gmail.com>
2022-06-23 14:12:25 +0100

    added GraphQLID

--- schema/schema.js
+++ schema/schema.js
@@ -1,20 +1,44 @@
 import graphql from "graphql"
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLID,
+    GraphQLInt
+} = graphql;
 
-// dummy data
+
+// *********** dummy data ***********
 var books = [
     { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
     { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
     { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
 ];
 
+var authors = [
+    { name: 'Patrick Rothfuss', age: 44, id: '1' },
+    { name: 'Brandon Sanderson', age: 42, id: '2' },
+    { name: 'Terry Pratchett', age: 66, id: '3' }
+];
+// ***********************************
+
+
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
     })
 });
 
@@ -23,12 +47,22 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            args: { id: { type: GraphQLString } },
+            args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 // code to get data from db / other source
                 return books.find(element => element.id === args.id);
             }
-        }
+        },
+        
+        author: {
+            type: AuthorType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                // code to get data from db / other source
+                return authors.find(element => element.id === args.id);
+            }
+        },
+    
     }
 });
 
